8f9492a0bd10616f8cce61b5e0a3fad7b0dc8483
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2020-01-09 19:46:02 +0100

    stuff

--- src/client/components/agenda.jsx
+++ src/client/components/agenda.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bilde from "../img/agenda.jpg";
 
 class Agenda extends React.Component{
 
@@ -9,15 +10,18 @@ class Agenda extends React.Component{
     render(){
         return(
             <div className="content2 agenda">
-                <h3>Fredag 26. juni</h3>
-                    <p>18:00 - ut kvelden: Grilling og hygge ved Fjellstua</p>
-                <h3>Lørdag 27. juni</h3>
-                    <p>13:00 Vielse i Rauland kirke</p>
-                    <p>15:00 Appretiff og fingermat ved Lognvik Fjellstue</p>
-                    <p>17:00 Middag på Lognvik Fjellstue</p>
-                <h3>Søndag 28. juni</h3>
-                    <p>Frokost</p>
-                    <p>Avreise</p>
+                <img src={Bilde} id={"img"} align={"center"} />
+                <div className="info">
+                    <h3>Fredag 26. juni</h3>
+                        <p>18:00 - ut kvelden: Grilling og hygge ved Fjellstua</p>
+                    <h3>Lørdag 27. juni</h3>
+                        <p>13:00 Vielse i Rauland kirke</p>
+                        <p>15:00 Appretiff og fingermat ved Lognvik Fjellstue</p>
+                        <p>17:00 Middag på Lognvik Fjellstue</p>
+                    <h3>Søndag 28. juni</h3>
+                        <p>Frokost</p>
+                        <p>Avreise</p>
+                </div>
             </div>
         )
     }
--- src/client/components/main.jsx
+++ src/client/components/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ForsideBilde from '../img/forside.jpg';
+import {Link} from "react-router-dom";
 
 export default class Main extends React.Component{
 
@@ -13,9 +14,9 @@ export default class Main extends React.Component{
             <p>Vi gifter oss lørdag 27. juni 2020 og vi vil gjerne dele
               helgen 26-28. juni med deg!
             </p>
-            <p>Gå til "Kommer du" for å svare på om du kommer eller ikke.
+              <p>Gå til <Link className="inline-link" to="/rsvp"><i>kommer du?</i></Link> for å svare på om du kommer eller ikke.
               Vi vil veldig gjerne ha svar fra deg om du kommer innen 1. April</p>
-              <p>For mer informasjon om dagen, se praktisk info og </p>
+              <p>For mer informasjon om dagen, se <Link className="inline-link" to="/rsvp"><i>praktisk info</i></Link>.</p>
           </div>
       </div>
     )
