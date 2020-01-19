f96762e3ffe98177da7284814177198d71b4cd32
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2020-01-19 14:22:23 +0100

    Font fiks

--- src/client/components/lognvik.jsx
+++ src/client/components/lognvik.jsx
@@ -11,81 +11,29 @@ export default class Lognvik extends React.Component {
     render() {
         return (
             <div className="content2 lognvik">
-                <h2>Lognvik Søndre</h2>
                 <div className="paragraf">
-                    <div>
-                        <p>Lognvik betyr "den lugne viken" og har gitt navn til to gårder som ligger langs Lognvikvatn, øst for
-                            Totak i sørenden av Hardangervidda, 750 moh. Husene er hovedsakelig fra 16- og 17-hundretallet.
+                    <img src={Lognvik2} id={"lognvik2"} align={"center"}/>
+                    <div className="text">
+                        <h2>Lognvik Søndre</h2>
+                        <p>Lognvik Søndre Gård er familien til Magnus sin gård i Rauland i Telemark fra 16- og 17-hundretallet. Gården ble opprinnelig kjøpt av Magnus sin oldefar Harald Didrik Kongshem i år 1933. Etter dette var det Magnus sin farfar, Torstein Halvor Kongshem, som overtok etter sin far. I dag er det faren til Magnus, Torstein Didrik Kongshem som eier og driver gården.
+                        </p>
+                        <p>
+                            Lognvik betyr "den lugne viken" og har i hele Magnus sitt liv vært hans kjæreste ferie-paradis. Så fort annledningen har bydd seg har Magnus hatt et ønske om å tilbringe tid på denne fantastiske plassen. Det har vært veldig viktig for Magnus å dele denne plassen med sine nærmeste, så hver sommer de siste 10 årene har det vært en årlig vennetur til Lognvik.
+                        </p>
+                        <p>
+                            Kathrine ble tidlig introdusert for Lognvik og tilbragte sin første påskeferie sammen med Magnus på Lognvik. Med årene har Kathrine og Magnus brukt mye av ferien sin her oppe, da det tilbyr veldig fine turmuligheter både med og uten ski for dem alle 3.
                         </p>
                         <p>
                             Lognvik Søndre er en landbrukseiendom bestående av omtrent 6600 dekar landområde fordelt på dyrket
                             og udyrket mark, skog og fjell, samt 25 bygninger – inkludert en fjellstue, (Lognvik Fjellstue). De
-                            fleste bygninger leies ut på åremål eller i ferieperioder. Nåværende eier, Torstein Didrik Kongshem
-                            (f. 1963), er tredje generasjon Kongshem som eier gården. Torstein D. tok over etter sin far,
-                            Torstein Halvor Kongshem (1928 – 2006). Han hadde selv tatt over etter sin far, Harald Didrik
-                            Kongshem (født Didrik Halvorsen), som kjøpte gården i 1933. På den tiden var den i meget dårlig
-                            forfatning og Didrik gjennomførte derfor en rekke oppussings- og reparasjonsarbeider. Han tok også
-                            på seg å sette istand bur og loft som han kom over på sine reiser i øst- og vest-Telemark, og
-                            flyttet de til gården. Ett av disse var det såkalte Volltveitloftet fra hans farmors slektsgård
-                            Volltveit i Sauherad. Loftet stammer fra middelalderen, og ble restaurert. I 1961 ble loftet gitt
-                            som gave til Telemark museum i Skien av hans sønn, Torstein Halvor Kongshem, til minne om sin far og
-                            i anledning museets 50-års jubileum.
+                            fleste bygninger leies ut på åremål eller i ferieperioder. Tunet på gården (12 bygninger) er fredet av Riksantikvaren. Veien mellom Øyfjell og Rauland deler tunet i to, der 10 av bygningene står på sørsiden og 2 på nordsiden.
                         </p>
                         <p>
-                            Et annet var Torvetjønnstua som opprinnelig kom fra fjellgården Torvetjønn ved Møsvatn. Denne stua
-                            ble, i likhet med et loft som var en kopi av Torvetjønnloftet, senere solgt (1961). Stua står nå på
-                            Neset ved Totakstrondi mens kopien av loftet fra Torvetjønn skal iflg. lokalhistoriker Johan Vaa
-                            befinne seg på Uppheim i Nesland,
-                        </p>
-                        <p>
-                            Mang en turist fra inn- og utland har opp igjennom årene gjestet gården for korte eller lengre
-                            perioder på jakt, fiske, tur, eller bare avslapning i en atmosfære av kultur og tradisjon.
-                        </p>
-                        <p>
-                            Tunet på gården (12 bygninger) er fredet av Riksantikvaren. Veien mellom Øyfjell og Rauland deler
-                            tunet i to, der 10 av bygningene står på sørsiden og 2 på nordsiden.
-                        </p>
-                        <p>
-                            Gården har en lang historie som det bl.a. redegjøres for i Rikard Berge's skrift "Lognvik. Ein
-                            ættegard og ein Telemarks-kunstnar." Norsk folkekulturs forlag, Skien 1934. Etterfølgende
-                            informasjon er hentet derfra:
+                            Den største bygningen på tunet heter storstua og var opprinnelig bygget med kun 1 etasje. Andre etasje ble påbygget i ettertid og var i sin tid den første "yvistoge" i Rauland. Yvistoge er utsmykket i fantastiske rosemalinger av Olav Torgjusson, en kunstner fra Uppigard Groven i Åmotsdal. I sin tid sov folkefortelleren Jørgen Moe i en av himmelsengene i yvistoge på Storstua.
                         </p>
 
                     </div>
-                    <img src={Lognvik2} id={"lognvik2"} align={"center"}/>
                 </div>
-                <p>
-                    Søndre og Nordre Lognvik var èn gård frem til 1600-tallet, men som Rikard Berge skriver "...alt fyrr
-                    1700 hev me Sudigard og Nordigard Lognvik, kvar på 2 tunnur". Eieren ved delingen var Hallvord
-                    Aslaksson. Han kom fra en gård ikke langt fra Lognvik ; "..den gamle og gjæve ætti i Gardsgjorde paa
-                    Haddland i Raudland, fødd ikr. 1680". Han giftet seg flere ganger og fikk 2 sønner hvorav en het
-                    Aslak.
-                    Hallvord Aslaksson hadde i tillegg gårdene Lognvik-Øygarden i Rauland, Aamot og Øvrebø og Moen i
-                    Åmotsda. Etter farens død i 1765 overtok Aslak Søndre Lognvik i en alder av 56 år.
-                </p>
-                <p>
-                    Aslak giftet seg med ei jente fra Rovshus i Rauland, og døde på Søndre Lognvik i 1779. Hans sønn,
-                    Anund
-                    Lognvik f.1749, giftet seg samme året som faren døde med Birgit Steinarsdottir Skeide, fra Flatdal.
-                    De
-                    ble viet på gården en vårdag.
-                </p>
-                <p>
-                    Anund bygde på hovedhuset Storstua til 2 høyder. Dette ble den første "yvistoge" i Rauland. Yvistoga
-                    ble utsmykket av Olav Torgjusson, en kunstner fra Uppigard Groven i Åmotsdal, f.1754. Yvistoga stod
-                    ferdig i 1787. Anund fikk derefter premie av "Det kgl. danske landhusholdningsselskab" i 1790 for
-                    drift av gården. Søndre Lognvik gikk videre i arv fra far til sønn; sønnesønn av Anund het Øystein,
-                    og på hans tid sov folkefortelleren Jørgen Moe i en av de gjeve utsmykkede himmelsenger i Yvistoga.
-                </p>
-                <p>
-                    Det har stått enda eldre loft på Søndre Lognvik men som senere er flyttet. En av disse, også ett
-                    jardloft, hadde en seng med årstall 1514 som også antas å være alderen på loftet. I 1712 solgte
-                    Hallvord Aslaksson loftet til "Sollid-Borken" i Åmotsdal. Loftet er derfor i ettertid blitt hetende
-                    Solli-loftet. Sollid-Borken het egentlig Hallvord Aslesson. Om "Sollid-Borken" skriver Rikard Berge
-                    at han var "...ein grimsk kar og manndraapar". Loftet står nå på Telemark Museum i Skien sammen med
-                    Volltveitloftet. Hallvord Aslaksson satte opp et nytt loft etter salget til Sollid-Borken. Det
-                    loftet står på tunet på Lognvik Søndre den dag i dag.
-                </p>
             </div>
         )
     }
