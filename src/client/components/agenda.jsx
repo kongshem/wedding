f85b81308f40c479c859c555f1d397c658553beb
KONGSHEM Magnus <mkongshe@one.steria.dom>
2020-02-10 11:35:45 +0100

    Fikset litt kolon i agenda

--- src/client/components/agenda.jsx
+++ src/client/components/agenda.jsx
@@ -16,13 +16,13 @@ class Agenda extends React.Component{
                         <p>Kl 18:00 - ut kvelden: Grilling og hygge ved Fjellstua</p>
                         <p><i>Antrekk: Sommerpent</i></p>
                     <h3>Lørdag 27. juni</h3>
-                        <p>Kl 13:00 Vielse i Rauland kirke</p>
+                        <p>Kl 13:00: Vielse i Rauland kirke</p>
                         <p><i>Antrekk: Bunad/Smoking/mørk dress</i></p>
-                        <p>Kl 15:00 Aperitiff og fingermat ved Lognvik Fjellstue</p>
+                        <p>Kl 15:00: Aperitiff og fingermat ved Lognvik Fjellstue</p>
                         <p><i>Antrekk: Smoking/mørk dress</i></p>
-                        <p>Kl 17:00 Middag og fest ved Lognvik Fjellstue</p>
+                        <p>Kl 17:00: Middag og fest ved Lognvik Fjellstue</p>
                     <h3>Søndag 28. juni</h3>
-                        <p>Kl 09:00-12:00 Frokost på Lognvik Fjellstue</p>
+                        <p>Kl 09:00-12:00: Frokost på Lognvik Fjellstue</p>
                 </div>
             </div>
         )
