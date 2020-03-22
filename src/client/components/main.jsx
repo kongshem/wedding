bcf861ecc540dda4940c894063d2a6a25eaaaebf
KONGSHEM Magnus <mkongshe@one.steria.dom>
2020-03-22 17:10:06 +0100

    Corona text

--- src/client/components/main.jsx
+++ src/client/components/main.jsx
@@ -2,27 +2,35 @@ import React from 'react';
 import ForsideBilde from '../img/bryllup2_resized.jpg';
 import {Link} from "react-router-dom";
 
-export default class Main extends React.Component{
+export default class Main extends React.Component {
 
-  render(){
+    render() {
 
-    return(
-      <div className="welcome">
-          <img src={ForsideBilde} id={"mainBilde"} align={"center"} />
-          <div className="info">
-            <h3 className="title">Vi gifter oss!</h3>
-            <p>Vi gifter oss lørdag 27. juni 2020 og vi vil gjerne dele
-              helgen 26-28. juni med deg!
-            </p>
-              <p>Vi ønsker å samle familie og gode venner til en bryllupshelg på Lognvik. Vi ser for oss en
-                  uformell fest fredag ettermiddag og kveld, en fin vielse i Rauland kirke med påfølgende
-                  bryllupsmiddag og -fest på Lognvik lørdag. Søndag runder vi av med en rolig frokost.
-              </p>
-              <p>Vi håper du har holdt av helgen. Gå til <Link className="inline-link" to="/rsvp"><i>kommer du</i></Link> for å svare.
-                  Vi vil veldig gjerne høre fra deg innen 1. April.</p>
-              <p>For mer informasjon om dagen, se <Link className="inline-link" to="/praktisk"><i>praktisk informasjon.</i></Link></p>
-          </div>
-      </div>
-    )
-  }
+        return (
+            <div className="welcome">
+                <img src={ForsideBilde} id={"mainBilde"} align={"center"}/>
+                <div className="info">
+                    <h3 className="title">Vi gifter oss!</h3>
+                    <p>Vi gifter oss lørdag 27. juni 2020 og vi vil gjerne dele
+                        helgen 26-28. juni med deg!
+                    </p>
+                    <p className="corona">
+                        Vi står nå i en vanskelig situasjon mtp Covid-19.
+                        Vi velger å ha is i magen og gjør ingen endringer for øyeblikket,
+                        men vi utvider svarfristen til 1. Mai. Vi følger situasjonen tett og vil fatte en
+                        beslutning når det nærmer seg.
+                    </p>
+                    <p>Vi ønsker å samle familie og gode venner til en bryllupshelg på Lognvik. Vi ser for oss en
+                        uformell fest fredag ettermiddag og kveld, en fin vielse i Rauland kirke med påfølgende
+                        bryllupsmiddag og -fest på Lognvik lørdag. Søndag runder vi av med en rolig frokost.
+                    </p>
+                    <p>Vi håper du har holdt av helgen. Gå til <Link className="inline-link" to="/rsvp"><i>kommer du</i></Link> for
+                        å svare.
+                        Vi vil veldig gjerne høre fra deg innen 1. Mai.</p>
+                    <p>For mer informasjon om dagen, se <Link className="inline-link" to="/praktisk"><i>praktisk
+                        informasjon.</i></Link></p>
+                </div>
+            </div>
+        )
+    }
 }
